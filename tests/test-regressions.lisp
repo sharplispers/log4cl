fc63ee5ffc1f4ef8b23c289fa083b7b91280036f
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-11-29 13:39:18 +0100

    tests: add tests for #8

--- tests/test-regressions.lisp
+++ tests/test-regressions.lisp
@@ -5,3 +5,21 @@
   "https://github.com/sharplispers/log4cl/issues/1"
   (log4cl:start-hierarchy-watcher-thread)
   (finishes (log4cl-impl::save-hook)))
+
+(deftest recursive-lock.issue#8.1 ()
+  "https://github.com/sharplispers/log4cl/issues/8"
+  (finishes
+    (with-simple-restart (abort "Abort")
+      (handler-bind
+          ((error (lambda (condition)
+                    (log:error "caught error: ~A" condition)
+                    (abort))))
+        (log:warn "~@{~A ~A~}" 1)))))
+
+(defun logging-function ()
+  (log:warn "I log, too")
+  :result)
+
+(deftest recursive-lock.issue#8.2 ()
+  "https://github.com/sharplispers/log4cl/issues/8"
+  (finishes (log:warn "~A" (logging-function))))
