b8c86e1c79e19911fa118378e5133d894939b0b0
Frank Goenninger <frank.goenninger@goenninger.net>
2020-05-25 19:51:15 +0200

    Added support for syslog appender on AllegroCL.

--- src/appender/syslog-appender.lisp
+++ src/appender/syslog-appender.lisp
@@ -11,11 +11,14 @@
   ((name :initarg :name :type string
          :accessor syslog-appender-name)
    (include-pid? :initarg :include-pid? :type boolean
-                 :accessor syslog-appender-include-pid?))
+                 :accessor syslog-appender-include-pid?)
+   (facility :initarg :facility :type keyword
+	     :accessor syslog-appender-facility))
   (:default-initargs
    :layout (make-instance 'pattern-layout :conversion-pattern "%m")
     :name (lisp-implementation-type)
-    :include-pid? t)
+    :include-pid? t
+    :facility :local0)
   (:documentation
    "An appender that writes log messages to the syslog.
 
@@ -26,9 +29,13 @@ initarg and defaults to
 
 The :include-pid? initarg controls whether log entries produced by the
 syslog connection should include the process id (PID). The default is
-true."))
+true.
+
+The :facility initarg provides the syslog facility as a keyword (:local0 ..
+local7)."))
 
 (defmethod property-alist ((instance syslog-appender))
   (append (call-next-method)
           '((:name name :string-skip-whitespace)
-            (:include-pid? include-pid? boolean))))
+            (:include-pid? include-pid? boolean)
+	    (:facility facility keyword))))
